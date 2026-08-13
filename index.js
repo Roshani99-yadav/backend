7047fb766a27dbcc62f3b0fc5d421d24486f709f
roshaniydv1999 <roshaniydv1999@gmail.com>
2026-08-14 05:09:34 +0530

    update index

--- index.js
+++ index.js
@@ -184,3 +184,7 @@ app.listen(PORT, async () => {
   console.log(`Aarvisac Control server listening at http://localhost:${PORT}`);
   await initDb();
 });
+app.listen(PORT, async () => {
+  console.log(`Arvisac Control server listening at http://localhost:${PORT}`);
+  await initDb();
+});
